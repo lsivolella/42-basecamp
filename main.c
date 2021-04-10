ecd6546574c0bc55042ac4d953f841f20b2a262f
lgoncalv <lgoncalv@student.42sp.org.br>
2021-04-10 22:47:12 +0000

    prevencao contra erros finalizada e primeira iteracao da matriz concluida

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 /*   By: lgoncalv <lgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 14:31:05 by lgoncalv          #+#    #+#             */
-/*   Updated: 2021/04/10 19:14:26 by lgoncalv         ###   ########.fr       */
+/*   Updated: 2021/04/10 22:45:33 by lgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void ft_check_column_possibilities(int x, int y, int startindex);
+void ft_check_column_possibilities(char x, char y, int startindex, int *g_cells);
 
-char *g_cells;
+//int *g_cells;
 
 int main(int argc, char* argv[])
 {
@@ -26,46 +26,64 @@ int main(int argc, char* argv[])
 	int o;
 	int p;
 	char *param;
+	char *pargv;
+	int *g_cells;
 
-	i = 1;
+	i = 0;
 	j = 0;
+	m = 0;
+	n = 0;
+	o = 0;
 	p = 0;
 	param = (char *)malloc(17);
-	g_cells = (char *)malloc(64);
+	g_cells = (int *)malloc(64 * 4);
+	pargv = argv[1];
 	printf("%d\n", argc);
-	// Prevenção a erro não funciona.
-	if (argc > 16 && argc < 18)
+	if (argc > 1 && argc < 3 && *pargv != '\0')
 	{
-		for (i = 1; i < argc; i++)
+		while (*pargv)
 		{
-			//printf("argv[%d] = %s\n", argc, argv[i]);
-			if (*argv[i] < '1' || *argv[i] > '9')
+			if (*pargv >= '1' && *pargv <= '4')
 			{
-				printf("Error\n");
-				return (0);
+				param[i] = *pargv;
+				i++;
+				pargv++;
+				j++;
+				if (*pargv != ' ' && i < 16)
+				{
+					printf("Error 1\n");
+					return (0);
+				}
 			}
 			else
 			{
-				param[i - 1] = *argv[i];
+				printf("Error 2\n");
+				return (0);
 			}
+			j++;
+			if (j > 31 && *pargv != '\0')
+			{
+				printf("Error 3\n");
+				return (0);
+			}
+			if (*pargv == '\0')
+				break ;
+			pargv++;
 		}
-		for (i = 0; i < 16; i++)
-		{
-			printf("[%c], ", param[i]);
-		}
+		param[i] = '\0';
 	}
-	else
+	for (i = 0; i < 16; i++)
 	{
-		printf("Error\n");
+		printf("[%c], ", param[i]);
 	}
 	printf("\n");
-
+	j = 0;
 	// Primeira análise: criar a árvore de possibilidade das colunas
-	while (j < 5)
+	while (j < 4)
 	{
 		// os vetores das celulas precisam ser variaveis globais
 		// para facilitar o acesso por todas as funções
-		ft_check_column_possibilities(param[j], param[j + 4], j);
+		ft_check_column_possibilities(param[j], param[j + 4], j, g_cells);
 		j++;
 	}
 	while (m < 4)
@@ -76,42 +94,46 @@ int main(int argc, char* argv[])
 			o = 0;
 			while (o < 4)
 			{
-				printf("%d/", g_cells[p]);
+				if (g_cells[p] != 100)
+				{
+					printf("%d/", g_cells[p]);
+				}
 				p++;
 				o++;
 			}
 			n++;
-			printf(", ");
+			printf(",    ");
 		}
 		m++;
+		printf("\n");
 		printf("\n");
 	}
 	return (0);
 }
 
-void ft_check_column_possibilities(int x, int y, int startindex)
+void ft_check_column_possibilities(char x, char y, int startindex, int *g_cells)
 {
 	int index;
 
-	if (startindex == 1)
+	if (startindex == 0)
 	{
 		// usar matrixcells[0] a matrixcells[3]
 		index = 0;
 	}
-	else if (startindex == 2)
+	else if (startindex == 1)
 	{
 		// usar matrixcells[4] a matrixcells[7]
-		index = 16;
+		index = 4;
+	}
+	else if (startindex == 2)
+	{
+		// usar matrixcells[8] a matrixcells[11]
+		index = 8;
 	}
 	else if (startindex == 3)
 	{
-		// usar matrixcells[8] a matrixcells[11]
-		index = 32;
-	}
-	else if (startindex == 4)
-	{
 		// usar matrixcells[12] a matrixcells[15]
-		index = 48;
+		index = 12;
 	}
 	if (x == '1')
 	{
@@ -130,22 +152,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 4; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 1; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 0; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 1; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 0; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 1; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 0; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 1; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 0; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 0; //Equivale ao predio 1
-			g_cells[index + 13] = 0; //Equivale ao predio 2
-			g_cells[index + 14] = 3; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 0; //Equivale ao predio 1
+			g_cells[index + 49] = 0; //Equivale ao predio 2
+			g_cells[index + 50] = 3; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 		else if (y == '3')
 		{
@@ -156,22 +178,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 4; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 1; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 3; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 1; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 3; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 1; //Equivale ao predio 1
-			g_cells[index + 9] = 0; //Equivale ao predio 2
-			g_cells[index + 10] = 3; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 1; //Equivale ao predio 1
+			g_cells[index + 33] = 0; //Equivale ao predio 2
+			g_cells[index + 34] = 3; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 1; //Equivale ao predio 1
-			g_cells[index + 13] = 2; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 1; //Equivale ao predio 1
+			g_cells[index + 49] = 2; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 		else if (y == '4')
 		{
@@ -182,22 +204,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 4; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 0; //Equivale ao predio 1
-			g_cells[index + 5] = 0; //Equivale ao predio 2
-			g_cells[index + 6] = 3; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 0; //Equivale ao predio 1
+			g_cells[index + 17] = 0; //Equivale ao predio 2
+			g_cells[index + 18] = 3; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 0; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 0; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 0; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 0; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 1; //Equivale ao predio 1
-			g_cells[index + 13] = 0; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 1; //Equivale ao predio 1
+			g_cells[index + 49] = 0; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 	}
 	else if (x == '2')
@@ -212,22 +234,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 1; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 0; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 1; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 0; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 1; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 0; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 1; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 0; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 0; //Equivale ao predio 1
-			g_cells[index + 13] = 0; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 4; //Equivale ao predio 4
+			g_cells[index + 48] = 0; //Equivale ao predio 1
+			g_cells[index + 49] = 0; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 4; //Equivale ao predio 4
 		}
 		else if (y == '2')
 		{
@@ -238,22 +260,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 1; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 0; //Equivale ao predio 3
-			g_cells[index + 7] = 4; //Equivale ao predio 4
+			g_cells[index + 16] = 1; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 0; //Equivale ao predio 3
+			g_cells[index + 19] = 4; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 1; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 0; //Equivale ao predio 3
-			g_cells[index + 11] = 4; //Equivale ao predio 4
+			g_cells[index + 32] = 1; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 0; //Equivale ao predio 3
+			g_cells[index + 35] = 4; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 1; //Equivale ao predio 1
-			g_cells[index + 13] = 2; //Equivale ao predio 2
-			g_cells[index + 14] = 3; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 1; //Equivale ao predio 1
+			g_cells[index + 49] = 2; //Equivale ao predio 2
+			g_cells[index + 50] = 3; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 		else if (y == '3')
 		{
@@ -264,22 +286,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix 1, 2
-			g_cells[index + 4] = 0; //Equivale ao predio 1
-			g_cells[index + 5] = 0; //Equivale ao predio 2
-			g_cells[index + 6] = 0; //Equivale ao predio 3
-			g_cells[index + 7] = 4; //Equivale ao predio 4
+			g_cells[index + 16] = 0; //Equivale ao predio 1
+			g_cells[index + 17] = 0; //Equivale ao predio 2
+			g_cells[index + 18] = 0; //Equivale ao predio 3
+			g_cells[index + 19] = 4; //Equivale ao predio 4
 
 			// Matrix 1, 3
-			g_cells[index + 8] = 0; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 3; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 0; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 3; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix 1, 4
-			g_cells[index + 12] = 1; //Equivale ao predio 1
-			g_cells[index + 13] = 2; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 1; //Equivale ao predio 1
+			g_cells[index + 49] = 2; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 		else if (y == '4')
 		{
@@ -299,22 +321,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 1; //Equivale ao predio 1
-			g_cells[index + 5] = 0; //Equivale ao predio 2
-			g_cells[index + 6] = 3; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 1; //Equivale ao predio 1
+			g_cells[index + 17] = 0; //Equivale ao predio 2
+			g_cells[index + 18] = 3; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 1; //Equivale ao predio 1
-			g_cells[index + 9] = 2; //Equivale ao predio 2
-			g_cells[index + 10] = 3; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 1; //Equivale ao predio 1
+			g_cells[index + 33] = 2; //Equivale ao predio 2
+			g_cells[index + 34] = 3; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 0; //Equivale ao predio 1
-			g_cells[index + 13] = 0; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 4; //Equivale ao predio 4
+			g_cells[index + 48] = 0; //Equivale ao predio 1
+			g_cells[index + 49] = 0; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 4; //Equivale ao predio 4
 		}
 		else if (y == '2')
 		{
@@ -325,22 +347,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 0; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 3; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 0; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 3; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 0; //Equivale ao predio 1
-			g_cells[index + 9] = 0; //Equivale ao predio 2
-			g_cells[index + 10] = 0; //Equivale ao predio 3
-			g_cells[index + 11] = 4; //Equivale ao predio 4
+			g_cells[index + 32] = 0; //Equivale ao predio 1
+			g_cells[index + 33] = 0; //Equivale ao predio 2
+			g_cells[index + 34] = 0; //Equivale ao predio 3
+			g_cells[index + 35] = 4; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 1; //Equivale ao predio 1
-			g_cells[index + 13] = 2; //Equivale ao predio 2
-			g_cells[index + 14] = 3; //Equivale ao predio 3
-			g_cells[index + 15] = 0; //Equivale ao predio 4
+			g_cells[index + 48] = 1; //Equivale ao predio 1
+			g_cells[index + 49] = 2; //Equivale ao predio 2
+			g_cells[index + 50] = 3; //Equivale ao predio 3
+			g_cells[index + 51] = 0; //Equivale ao predio 4
 		}
 		else if (y == '3')
 		{
@@ -365,22 +387,22 @@ void ft_check_column_possibilities(int x, int y, int startindex)
 			g_cells[index + 3] = 0; //Equivale ao predio 4
 
 			// Matrix x, 2
-			g_cells[index + 4] = 0; //Equivale ao predio 1
-			g_cells[index + 5] = 2; //Equivale ao predio 2
-			g_cells[index + 6] = 0; //Equivale ao predio 3
-			g_cells[index + 7] = 0; //Equivale ao predio 4
+			g_cells[index + 16] = 0; //Equivale ao predio 1
+			g_cells[index + 17] = 2; //Equivale ao predio 2
+			g_cells[index + 18] = 0; //Equivale ao predio 3
+			g_cells[index + 19] = 0; //Equivale ao predio 4
 
 			// Matrix x, 3
-			g_cells[index + 8] = 0; //Equivale ao predio 1
-			g_cells[index + 9] = 0; //Equivale ao predio 2
-			g_cells[index + 10] = 3; //Equivale ao predio 3
-			g_cells[index + 11] = 0; //Equivale ao predio 4
+			g_cells[index + 32] = 0; //Equivale ao predio 1
+			g_cells[index + 33] = 0; //Equivale ao predio 2
+			g_cells[index + 34] = 3; //Equivale ao predio 3
+			g_cells[index + 35] = 0; //Equivale ao predio 4
 
 			// Matrix x, 4
-			g_cells[index + 12] = 0; //Equivale ao predio 1
-			g_cells[index + 13] = 0; //Equivale ao predio 2
-			g_cells[index + 14] = 0; //Equivale ao predio 3
-			g_cells[index + 15] = 4; //Equivale ao predio 4
+			g_cells[index + 48] = 0; //Equivale ao predio 1
+			g_cells[index + 49] = 0; //Equivale ao predio 2
+			g_cells[index + 50] = 0; //Equivale ao predio 3
+			g_cells[index + 51] = 4; //Equivale ao predio 4
 		}
 		else if (y == '2')
 		{
